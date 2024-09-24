309eaff1c1cb5a6ab839532a6501f46ee65b8691
aziiemuth <145832886+aziiemuth@users.noreply.github.com>
2024-09-24 10:09:17 +0700

    Add files via upload

---
+++ Pertemuan 2/perulangan 1.py
@@ -0,0 +1,10 @@
+# Input jumlah total belanja
+total_belanja = float(input("Masukkan total belanja Anda: "))
+
+# Mengecek apakah total belanja lebih dari 100.000
+if total_belanja > 100000:
+    print("Selamat, Anda dapat hadiah!")
+else:
+    print("Terimakasih sudah berbelanja")
+
+print("Selesai")
---
+++ Pertemuan 2/perulangan 2.py
@@ -0,0 +1,14 @@
+# Password yang benar
+password_benar = "12345"
+
+# Input password dari pengguna
+password = input("Masukkan password Anda: ")
+
+# Mengecek apakah password yang dimasukkan benar
+if password == password_benar:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+
+print("Terimakasih sudah menggunakan aplikasi kami")
+print("Selesai")
